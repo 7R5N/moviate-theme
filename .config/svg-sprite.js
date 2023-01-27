65b644e81ec10326eb96ba7613d510ce35a45f34
Lars Fuchs <larsen@web.de>
2023-01-27 18:44:29 +0100

    Add missing .config folder

---
+++ .config/svg-sprite.js
@@ -0,0 +1,11 @@
+// Common svg-sprite config options and their default values
+const config = {
+	dest: './_img', // Main output directory
+	log: debug, // Logging verbosity (default: no logging)
+	mode: {
+	//	inline: true, // Prepare for inline embedding
+		symbol: true, // Create a «symbol» sprite
+		dest: "./",
+		sprite: "octicons_.svg"
+	}
+}
