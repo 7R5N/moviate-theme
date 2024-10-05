ce62a53c775cdc7bcdd7323bb125914f52fcde40
Lars Fuchs <lars.fuchs@wysiwyg.de>
2024-10-06 01:04:28 +0200

    update
    update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '7r5n/moviate-theme',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '619c5a0541a66a611a04046b7b4a4849b99e9a5c',
+        'type' => 'automad-package',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '7r5n/moviate-theme' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '619c5a0541a66a611a04046b7b4a4849b99e9a5c',
+            'type' => 'automad-package',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'automad/package-installer' => array(
+            'pretty_version' => '1.1.2',
+            'version' => '1.1.2.0',
+            'reference' => '6cefe27a0b7f6fc3432d08e8eeed9e6f6aac0e8b',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/../automad/package-installer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
